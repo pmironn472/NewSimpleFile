61372247ab08f64ff26ec8dc497c5620e6363036
pmironn472 <63416798+pmironn472@users.noreply.github.com>
2020-05-20 22:30:32 +0300

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,63 @@
+
+import java.io.*;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+
+        writeText();
+
+        readText();
+
+
+
+
+
+    }
+
+    public static void writeText()  {
+
+        int lines = 4;
+        try {
+
+            BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter pw = new BufferedWriter(new FileWriter("Orderss.txt"));
+            
+            String data;
+
+            while(lines != 0 ){
+                data = br.readLine();
+                pw.write(data + "\n");
+                pw.flush();
+                lines--;
+            }
+
+
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+
+    public static void readText() {
+        try {
+            File file = new File("Orders.txt");
+                Scanner in = new Scanner(file);
+                for (int i = 0; i < 4; i++) {
+                    String data = in.nextLine();
+                    System.out.println(data);
+                }
+        }catch (FileNotFoundException e){
+            System.out.println("Error, file not found");
+        }catch (NoSuchElementException e){
+            System.out.println("No data");
+        }finally {
+            System.out.println();
+            System.out.println("Thanks , for using our app");
+        }
+
+
+    }
+}
